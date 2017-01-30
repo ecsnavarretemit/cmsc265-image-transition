93af6e846b794cc2a5ebd9ee572218fb79dcf048
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-30 21:34:30 +0800

    Create external function for creating image sequences

--- src/index.js
+++ src/index.js
@@ -16,6 +16,47 @@ const validateDimensions = require('./image-processor/validate-dimensions');
 
 const resolvedPath = path.join(config.common.src, 'assets/img');
 
+const createSequence = (cvImages, outputDir) => {
+  const numExec = cvImages.length - 1;
+  const numImagesPerExec = 2;
+  const numIterations = 10;
+
+  // delete the existing putput directory
+  if (fs.existsSync(outputDir)) {
+    console.log(`Deleting old output directory: ${outputDir}`);
+
+    del.sync([outputDir]);
+  }
+
+  // show the path in the console
+  console.log(`Creating output directory: ${outputDir}`);
+
+  // create output directory
+  fs.mkdirSync(outputDir);
+
+  let imageCtr = 0;
+  for (let i = 0; i < numExec; i += 1) {
+    const imgSet = cvImages.slice(i, numImagesPerExec + i);
+    let k = 0;
+
+    for (let j = numIterations; j >= 0; j -= 1) {
+      const fadein1 = j / parseFloat(`${numIterations}`);
+      const fadein2 = k / parseFloat(`${numIterations}`);
+
+      const result = new cv.Matrix(imgSet[0].width(), imgSet[0].height());
+      result.addWeighted(imgSet[0], fadein1, imgSet[1], fadein2, 0);
+      result.save(path.join(outputDir, `morph-${imageCtr}.jpg`));
+
+      imageCtr += 1;
+      k += 1;
+    }
+  }
+
+  // show the message to the console
+  console.log('Finished processing images.');
+};
+
+// start the promise execution.
 fetchAll(resolvedPath)
   .then((images) => {
     // create an array of image promises
@@ -25,45 +66,7 @@ fetchAll(resolvedPath)
     return Promise.all(promises);
   })
   .then(cvImages => validateDimensions(cvImages))
-  .then((cvImages) => {
-    const numExec = cvImages.length - 1;
-    const numImagesPerExec = 2;
-    const numIterations = 10;
-
-    // delete the existing putput directory
-    if (fs.existsSync(config.common.out)) {
-      console.log(`Deleting old output directory: ${config.common.out}`);
-
-      del.sync([config.common.out]);
-    }
-
-    // show the path in the console
-    console.log(`Creating output directory: ${config.common.out}`);
-
-    // create output directory
-    fs.mkdirSync(config.common.out);
-
-    let imageCtr = 0;
-    for (let i = 0; i < numExec; i += 1) {
-      const imgSet = cvImages.slice(i, numImagesPerExec + i);
-      let k = 0;
-
-      for (let j = numIterations; j >= 0; j -= 1) {
-        const fadein1 = j / parseFloat(`${numIterations}`);
-        const fadein2 = k / parseFloat(`${numIterations}`);
-
-        const result = new cv.Matrix(imgSet[0].width(), imgSet[0].height());
-        result.addWeighted(imgSet[0], fadein1, imgSet[1], fadein2, 0);
-        result.save(path.join(config.common.out, `morph-${imageCtr}.jpg`));
-
-        imageCtr += 1;
-        k += 1;
-      }
-    }
-
-    // show the message to the console
-    console.log('Finished processing images.');
-  })
+  .then(cvImages => createSequence(cvImages, config.common.out))
   .catch(err => console.error(err))
   ;
 
