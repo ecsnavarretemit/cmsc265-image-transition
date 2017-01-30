d0c8f98f4773ad8f8766343e66d2299c99821a36
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-30 23:20:31 +0800

    Throw error when there is no image in the input directory

--- src/image-processor/fetch-all.spec.js
+++ src/image-processor/fetch-all.spec.js
@@ -12,12 +12,22 @@ const config = require('../config');
 
 describe('Fetch Images', () => {
   const resolvedPath = path.join(config.common.src, 'assets/img-test/same');
+  const noImagePath = path.join(config.common.src, 'assets/img-test');
   const nonExistentPath = path.join(config.common.src, 'non-existent-path');
 
-  it('should throw an error', (done) => {
+  it('should throw an error on non-existent directory', (done) => {
     fetchAll(nonExistentPath)
       .catch((err) => {
         expect(err.message).to.equal('Directory does not exist.');
+        done();
+      })
+      ;
+  });
+
+  it('should throw an error on directory that contains no images', (done) => {
+    fetchAll(noImagePath)
+      .catch((err) => {
+        expect(err.message).to.equal('Directory does not contain any images.');
         done();
       })
       ;
