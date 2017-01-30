a294b918fe25ddaadba1feff8240fd4e76840c2d
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-30 21:08:14 +0800

    Add unit test for reader module

---
+++ src/image-processor/reader.spec.js
@@ -0,0 +1,38 @@
+/*!
+ * Reader Test
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+const path = require('path');
+const expect = require('chai').expect;
+const cv = require('opencv');
+const reader = require('./reader');
+const config = require('../config');
+
+describe('Image Reader', () => {
+  const imagePath = path.join(config.common.src, 'assets/img-test/img-1.JPG');
+  const nonExistentImagePath = path.join(config.common.src, 'assets/img-test/non-existent.JPG');
+
+  it('should throw an error', (done) => {
+    reader(nonExistentImagePath)
+      .catch((err) => {
+        expect(err.message).to.equal('Directory does not exist.');
+        done();
+      })
+      ;
+  });
+
+  it('should create matrix instance', (done) => {
+    reader(imagePath)
+      .then((cvImage) => {
+        expect(cvImage).to.be.an.instanceof(cv.Matrix);
+
+        done();
+      })
+      ;
+  });
+});
+
+
