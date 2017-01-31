5b775d2975ba977a0809c2535ec787419e3ec868
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-31 22:34:34 +0800

    Add more comments in create-sequence module

--- src/image-processor/create-sequence.js
+++ src/image-processor/create-sequence.js
@@ -32,6 +32,7 @@ const createSequence = (cvImages, filenamePrefix, outputDir) => {
   // create output directory
   fs.mkdirSync(outputDir);
 
+  // loop through all cv image instances
   let imageCtr = 0;
   for (let i = 0; i < numExec; i += 1) {
     const imgSet = cvImages.slice(i, numImagesPerExec + i);
@@ -41,8 +42,14 @@ const createSequence = (cvImages, filenamePrefix, outputDir) => {
       const fadein1 = j / parseFloat(`${numIterations}`);
       const fadein2 = k / parseFloat(`${numIterations}`);
 
+      // create new matrix instance
       const result = new cv.Matrix(imgSet[0].width(), imgSet[0].height());
+
+      // process the opacity of first and second image by
+      // decreasing the opacity of the first and increasing the opacity of the next image
       result.addWeighted(imgSet[0], fadein1, imgSet[1], fadein2, 0);
+
+      // save the processed image in the output directory and name it sequentially.
       result.save(path.join(outputDir, templateFn({
         prefix: filenamePrefix,
         sequenceNumber: padStart((imageCtr + 1), 3, '0'),
